2a8d57adeb108c7f442220e944b36ea4fc09f488
mohankgandhi06 <mohankgandhi06@gmail.com>
2019-08-13 11:20:47 +0530

    Initial Versioning 0.1

---
+++ src/com/revision/ctci/ktesting/ETestAPen.java
@@ -0,0 +1,16 @@
+package com.revision.ctci.ktesting;
+
+public class ETestAPen {
+    /* Testing a Pen */
+    /* #1: What the pen is for? (Chief Purpose)
+     *       WRITING | DRAWING | PLAYING | COLORING
+     *     Who uses it? (Target Audience)
+     *       WRITER | ADULTS | OLD PEOPLE | CHILDREN
+     * #2: How should it operate? (Use Cases)
+     *       WRITE (to begin with) | AVAILABLE IN VARIOUS COLOR | AVAILABLE IN VARIOUS TYPES (Calligraphy, painting like B, HB, etc)
+     *       PLAYING (should it be strong, unbreakable, water resistant, heat resistant, ability to write upside down since usually pen
+     *       work on flow and that depends on gravity and so difficult when we are upside down fo some reason while writing :) )
+     *       SHARPNESS (since if children uses it should not be ideally sharp)
+     * #3: Extreme conditions acceptable for it (Load it can bear without breaking)
+     * #4: Failure Conditions (Scenarios when it starts to break)*/
+}
